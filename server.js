ad38ab58146cafdb58333590e2e6a141282d2eed
Sang Lee <sanglee76@yahoo.com>
2021-01-18 23:19:54 -0800

    updated locationset-up

--- server.js
+++ server.js
@@ -16,9 +16,40 @@ const PORT = process.env.PORT || 3111;
 
 
 //========== Setup Routes ============//
-app.get('/',(request, response) => {
-  response.send('you made it');
+app.get('/', (request, response) => {
+  response.send('you made it home');
 });
 
-//=========== Start Server ===========//
-app.listen(PORT, () => console.log(`we are up on PORT ${PORT}`));
+app.get('/location', (req, res) => {
+  const dataArrayFromTheLocationJson = require('./data/location.json');
+  const dataObjFromJson = dataArrayFromTheLocationJson[0];
+
+  const searchedCity = req.query.city; //comes from the front-end, req.query is the way we get data from the front-end.
+
+
+  const newLocation = new Location(
+    searchedCity,
+    dataObjFromJson.display_name,
+    dataObjFromJson.lat,
+    dataObjFromJson.lon
+  );
+  res.send(newLocation);
+});
+
+app.get('/weather', (req, res) => {
+  const data = require('./data/weather.json');
+  const
+});
+
+  //========= Helper Functions =========//
+  //Constructor goes here//
+
+  function Location(search_query, formatted_query, latitude, longitude) {
+    this.search_query = search_query;
+    this.formatted_query = formatted_query; // what is this? Need to revisit lecture video
+    this.longitude = longitude;
+    this.latitude = latitude;
+  }
+
+  //=========== Start Server ===========//
+  app.listen(PORT, () => console.log(`we are up on PORT ${PORT}`));
